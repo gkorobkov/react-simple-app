ead677036c8d980c276f9124df0d3fef487d9fd4
George K. <gkorobkov@gmail.com>
2025-04-13 18:07:22 +0300

    Add simple exapmles

--- App.js
+++ App.js
@@ -1,22 +1,81 @@
    // App.js
    import React from 'react';
-   export default App;
+   import { useState } from 'react';
 
-   function App() {
+   export default function App() {
      return (
-       <><h1>Hello, World! 13</h1><div>
-         <h1>Welcome to my app. v2</h1>
+       <>
+       <h1>Hello, World! 13</h1>
+       <div>
+         <h2>Welcome to my app. v3</h2>
          <MyButton />
-       </div></>       
+       </div>
+       <div>
+         <Profile />
+       </div>
+       <ShoppingList />
+       <MyButton />
+       </>       
      );
    }
 
    function MyButton() {
+    const [count, setCount] = useState(0);
+    
+    function handleClick() {
+      //alert('You clicked me!');
+      setCount(count + 1);
+    }
+  
     return (
-      <button>
-        I'm a button
+      <button onClick={handleClick}>
+        Click me. Clicked {count} times
       </button>
     );
   }
   
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
 
+  function Profile() {
+    return (
+      <>
+        <h1>Profile: {user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
+
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+  
+  function ShoppingList() {
+    const listItems = products.map(product =>
+      <li
+        key={product.id}
+        style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+      >
+        {product.title}
+      </li>
+    );
+  
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
--- dist/bundle.js
+++ dist/bundle.js
@@ -10,18 +10,74 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "default": () => (/* binding */ App)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 // App.js
 
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+
 function App() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Hello, World! 13"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Welcome to my app. v2"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(MyButton, null)));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Hello, World! 13"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h2", null, "Welcome to my app. v3"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(MyButton, null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Profile, null)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(ShoppingList, null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(MyButton, null));
 }
 function MyButton() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", null, "I'm a button");
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(0),
+    _useState2 = _slicedToArray(_useState, 2),
+    count = _useState2[0],
+    setCount = _useState2[1];
+  function handleClick() {
+    //alert('You clicked me!');
+    setCount(count + 1);
+  }
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", {
+    onClick: handleClick
+  }, "Click me. Clicked ", count, " times");
+}
+var user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+};
+function Profile() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Profile: ", user.name), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", {
+    className: "avatar",
+    src: user.imageUrl,
+    alt: 'Photo of ' + user.name,
+    style: {
+      width: user.imageSize,
+      height: user.imageSize
+    }
+  }));
+}
+var products = [{
+  title: 'Cabbage',
+  isFruit: false,
+  id: 1
+}, {
+  title: 'Garlic',
+  isFruit: false,
+  id: 2
+}, {
+  title: 'Apple',
+  isFruit: true,
+  id: 3
+}];
+function ShoppingList() {
+  var listItems = products.map(function (product) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("li", {
+      key: product.id,
+      style: {
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }
+    }, product.title);
+  });
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("ul", null, listItems);
 }
 
 /***/ }),
