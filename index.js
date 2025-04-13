c888d5ac114ca8810ddb7238be3871f009825fea
George K. <gkorobkov@gmail.com>
2025-04-13 21:48:38 +0300

    try to fix error

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
+//window.React = React
+//import App from './App'; 
+
+const restEndpoint = "https://jsonplaceholder.typicode.com/posts/10";
+
+const callRestApi = async () => {
+      const response = await fetch(restEndpoint);
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      return jsonResponse;
+      return JSON.stringify(jsonResponse);
+      
+      const arrayOfLists = jsonResponse.records.map(
+        record => <li key={record.id}><b>{record.title}</b> </li>
+      )
+      return arrayOfLists;
+  };
+  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const myelement = <h1><u>These are the details of the myelement const</u></h1>;
 
-root.render(
-      <App />
-)
+//root.render(      <App />)
+function RenderResult() {
+      const [apiResponse, setApiResponse] = useState("*** now loading ***");
     
+      useEffect(() => {
+          callRestApi().then(
+              result => setApiResponse(result));
+      },[]);
+    
+      return(
+          <div>
+              <h1>React App</h1>
+              <div>{apiResponse}</div>
+          </div>
+      );
+    };
+
+root.render(<RenderResult/>);
+
+//https://jsonplaceholder.typicode.com/posts
