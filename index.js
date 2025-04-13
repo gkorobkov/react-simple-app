276130118207b9b5f558a35985f3d82b3222a218
George K. <gkorobkov@gmail.com>
2025-04-13 22:14:24 +0300

    Fix fetch items from API

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-//window.React = React
-//import App from './App'; 
 
-const restEndpoint = "https://jsonplaceholder.typicode.com/posts/10";
+//const getRecordsEndpoint = "https://jsonplaceholder.typicode.com/posts/";
 
-const callRestApi = async () => {
-      const response = await fetch(restEndpoint);
-      const jsonResponse = await response.json();
-      console.log(jsonResponse);
-      return jsonResponse;
-      return JSON.stringify(jsonResponse);
-      
-      const arrayOfLists = jsonResponse.records.map(
-        record => <li key={record.id}><b>{record.title}</b> </li>
-      )
-      return arrayOfLists;
-  };
-  
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const myelement = <h1><u>These are the details of the myelement const</u></h1>;
-
-//root.render(      <App />)
 function RenderResult() {
-      const [apiResponse, setApiResponse] = useState("*** now loading ***");
+      const [todos, setTodos] = useState([]);
     
       useEffect(() => {
-          callRestApi().then(
-              result => setApiResponse(result));
-      },[]);
+        const getTodos = async () => {
+          const response = await fetch(
+            'https://jsonplaceholder.typicode.com/users'
+          );
+          const data = await response.json();
+          console.log(data);
+          setTodos(data);
+        };
+        getTodos();
+      }, []);
     
-      return(
-          <div>
-              <h1>React App</h1>
-              <div>{apiResponse}</div>
-          </div>
+      console.log(todos);
+    
+      return (
+        <div>
+          {todos.map((todo) => (
+            <div key={todo.id}>
+              <h2>{todo.id}: {todo.name}</h2>
+            </div>
+          ))}
+        </div>
       );
-    };
+    }
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RenderResult/>);
-
-//https://jsonplaceholder.typicode.com/posts
